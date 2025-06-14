c2b359d045dbe3cc86e047bb0b186877d2b824a6
sahil_sohaib_ali <lihassohaib@gmail.com>
2025-06-15 01:14:51 +0530

    creating the product model by using mongoose and adding the structure of product controller- logic not added

---
+++ server/controllers/productController.js
@@ -0,0 +1,31 @@
+
+
+// add Product : /api/product/add
+export const addProduct = async (res , res)=>{
+    
+
+
+
+}
+
+
+//get Product : /api/product/list
+export const productList = async (req , res)=>{
+
+
+}
+
+
+
+//get Single Product : /api/product/id
+export const productById = async (req , res)=>{
+
+
+}
+
+
+//Change Product inStock : /api/product/stock
+export const changeStock = async (req , res)=>{
+
+
+}
